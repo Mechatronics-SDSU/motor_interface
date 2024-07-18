bc5ae07d7de29dc0a6cf0891cc6692ac370474db
kchan5071 <kchan5071@sdsu.edu>
2024-07-17 20:53:44 -0700

    change motor-interface to motors

--- MotorInterface.py
+++ MotorInterface.py
@@ -1,32 +1,19 @@
-from Motor.MotorWrapper import  Can_Wrapper
-from multiprocessing import     Process,    Value
+from Motor.MotorWrapper import Can_Wrapper
+from multiprocessing import Process, Value
 import math
 import time
-
-
+import numpy as np
 
 #from MotorWrapper import Can_Wrapper
 
 class MotorInterface:
 
-    '''
-        discord: @kialli
-        github: @kchan5071
-        
-        creates shared memory and processes to communicate between vision and control
-        
-        vision: writes to shared memory
-        currently just printing process data, later to use with PID control
-        
-        could use arrays, but prefer to use shared memory for simplicity and readability
-        
-    '''
-
-    def __init__(self, linear_acceleration_x ,  linear_acceleration_y,  linear_acceleration_z, 
-                angular_velocity_x,             angular_velocity_y,     angular_velocity_z, 
-                orientation_x,                  orientation_y,          orientation_z, 
+    def __init__(self, linear_acceleration_x , linear_acceleration_y, linear_acceleration_z, 
+                angular_velocity_x, angular_velocity_y, angular_velocity_z, 
+                orientation_x, orientation_y, orientation_z, 
                 depth,
-                offset_x,                       offset_y):
+                offset_x, offset_y,
+                dvl_z):
         self.linear_acceleration_x = linear_acceleration_x
         self.linear_acceleration_y = linear_acceleration_y
         self.linear_acceleration_z = linear_acceleration_z
@@ -39,76 +26,141 @@ class MotorInterface:
         self.depth = depth
         self.offset_x = offset_x
         self.offset_y = offset_y 
+        self.dvl_z = dvl_z
 
-        self.max_iterations = 10000
+        self.previous_x_offsets = []
+
+        self.max_iterations = 10000000
         self.can = Can_Wrapper()
 
-        #TUNE---------------------------------------------------------
-        self.x_hard_deadzone = 40
-        self.y_hard_deadzone = 40
+        #TUNE----------------------resize zed camera input-----------------------------------
+        self.x_hard_deadzone = 400
+        self.y_hard_deadzone = 400
 
-        self.x_soft_deadzone = 20
-        self.y_soft_deadzone = 20
+        self.x_soft_deadzone = 100
+        self.y_soft_deadzone = 200
 
-        self.x_turn_speed = .1
-        self.y_turn_speed = .1
+        self.x_turn_speed = 5
+        self.y_turn_speed = 5
+        self.normalizer_value = 640
 
-        self.depth_stop_value = 60
-        self.speed = .05
+        self.depth_stop_value = 1000
+        self.speed = 20
+        self.turn_down_speed = 10
+
+        self.iteration_since_last_detection = 0
 
 
-    def run_loop(self):
-        for i in range(self.max_iterations):
-            print(self.offset_x.value)
-            print(self.offset_y.value)
-            print(self.depth)
-            #no object
-            if (self.offset_x.value == 0 and self.offset_y.value == 0):
+    def follow(self):
+            start = time.time()
+            
+            #NO OBJECT -------------------------------------------------
+            if self.offset_x.value == 0:
+                self.iteration_since_last_detection += 1
                 self.can.stop()
             #HARD DEADZONE----------------------------------------------
             #turn right hard deadzone
-            if (self.offset_x.value > self.x_hard_deadzone):
-                self.can.turn_right(self.x_turn_speed)
+            #turn right if to the left of the hard deadzone
+            elif(self.offset_x.value < -self.x_hard_deadzone):
+                self.can.turn_right(abs(self.offset_x.value / self.normalizer_value * self.x_turn_speed))
+                self.iteration_since_last_detection = 0
             #turn left hard deadzone
-            if (self.offset_x.value < -self.x_hard_deadzone):
-                self.can.turn_left(self.x_turn_speed)
-            #turn up hard deadzone
-            if (self.offset_y.value > self.y_hard_deadzone):
-                self.can.turn_up(self.y_turn_speed)
-            
-            #turn down hard deadzone
-            if (self.offset_y.value < -self.y_hard_deadzone):
-                self.can.turn_down(self.y_turn_speed)
-            
+            #turn left if to the right of the hard deadzone
+            elif (self.offset_x.value > self.x_hard_deadzone):
+                self.can.turn_left(abs(self.offset_x.value / self.normalizer_value * self.x_turn_speed))
+                self.iteration_since_last_detection = 0
             #SOFT DEADZONE----------------------------------------------
-            #turn left soft deadzone
-            elif (self.offset_x.value < -self.x_soft_deadzone):
-                self.can.turn_left(self.x_turn_speed)
-                self.can.move_forward(self.speed)
-            
             #turn right soft deadzone
+            #turn right and move forward if to the left of the soft deadzone
+            elif (self.offset_x.value < -self.x_soft_deadzone):
+                self.can.turn_right(abs(self.offset_x.value / self.normalizer_value * self.x_turn_speed))
+                self.can.move_forward(self.speed)
+                self.iteration_since_last_detection = 0
+            #turn left soft deadzone
+            #turn left and move forward if to the right of the soft deadzone
             elif (self.offset_x.value > self.x_soft_deadzone):
-                self.can.turn_right(self.x_turn_speed)
+                self.can.turn_left(abs(self.offset_x.value / self.normalizer_value * self.x_turn_speed))
                 self.can.move_forward(self.speed)
-            
-            #turn up soft deadzone
-            if (self.offset_y.value > self.y_soft_deadzone):
-                self.can.turn_up(self.y_turn_speed)
+                self.iteration_since_last_detection = 0
+            #CENTERED---------------------------------------------------
+            #move forward if inside soft deadzone    
+            else: 
+                #print("centered")
                 self.can.move_forward(self.speed)
-            
-            #turn down soft deadzone
-            elif (self.offset_y.value < -self.y_soft_deadzone):
-                self.can.turn_down(self.y_turn_speed)
-            
+                self.iteration_since_last_detection = 0
+            #else: print(f"x: {self.offset_x.value} x_deadzone: {self.x_soft_deadzone}")
             #STOP DEPTH-----------------------------------------------
             #stop if depth is less than stop value
-            if (self.depth.value < self.depth_stop_value):
+            if (self.depth.value < self.depth_stop_value and self.depth.value != 0.0):
+                print("stop")
                 self.can.stop()
             
             #MOVE FORWARD---------------------------------------------
-            self.can.move_forward(self.speed)
             self.can.send_command()
+            end = time.time()
+            time.sleep(.05 - (end - start))
+
+    def move_down(self, down_time):
+        self.can.move_down(20)
+        self.can.send_command()
+        time.sleep(down_time)
+
+    def move_forward(self, forward_time):
+        self.can.move_forward(20)
+        self.can.move_down(.3)
+        self.can.send_command()
+        time.sleep(forward_time)
+
+    def look_for_detection(self):
+        #turn left if nothing in front
+        if self.offset_x.value == 0:
+        #if self.offset_x.value == 0:
+            self.can.turn_right(10) #lower turn speed?
+            self.can.send_command()
+            time.sleep(.3)
+            self.can.stop()
+            self.can.send_command()
+            time.sleep(1)
+        
+        if self.offset_x.value != 0:
+            self.iteration_since_last_detection = 0
+        else:
+            self.iteration_since_last_detection += 1
+
+    def sit_at_depth(self):
+        print(self.dvl_z.value)
+        if (self.dvl_z.value <= 0.0):
+            return
+        if self.dvl_z.value < .5:
+            self.can.move_down(.2)
+            print("down")
+            self.can.send_command()
+        elif self.dvl_z.value > 7:
+            self.can.move_up(.4)
+            print("up")
+            self.can.send_command()
+        pass
+
+    def run_loop(self):
+
+        # self.move_down(2)
+        # self.can.stop()
+        # self.can.send_command()
+
+        while True:
+            self.sit_at_depth()
             time.sleep(.05)
+            # if (self.iteration_since_last_detection < 20):
+            #     print(self.iteration_since_last_detection)
+            #     s            # if (self.iteration_since_last_detection < 20):
+            #     print(self.iteration_since_last_detection)
+            #     self.follow()
+            # else:
+            #     self.look_for_detection()
+            #     print(self.iteration_since_last_detection)elf.follow()
+            # else:
+            #     self.look_for_detection()
+            #     print(self.iteration_since_last_detection)
 
 
 if __name__ == '__main__':
@@ -131,7 +183,7 @@ if __name__ == '__main__':
                     depth=depth,                                                                                            #depth
                     offset_x=offset_x, offset_y=offset_y)  
                 
-    offset_x.value = 100.0
+    offset_x.value = 0.0
     depth.value = 100   
 
     interface.run_loop()
--- MotorWrapper.py
+++ MotorWrapper.py
@@ -30,15 +30,15 @@ class Can_Wrapper:
         self.motors = [
             #LjoyX   LjoyY   RjoyX   RjoyY    Rtrig   Ltrig   LPad       RDpad
             
-           #strafe for/bck   roll    ptch#     up#    down#   Lroll#     Rroll#
-            [ 0,      0,      -1,     -1,      -1,      1,      1,        -1], # motor 0 (top front left)
-            [ 1,     -1,       0,      0,       0,      0,      0,         0], # motor 1 (bottom front left)
-            [ 0,      0,      -1,      1,      -1,      1,      1,        -1], # motor 2 (top back left)
-            [-1,     -1,       0,      0,       0,      0,      0,         0], # motor 3 (bottom back left)
-            [ 0,      0,       1,      1,      -1,      1,     -1,         1], # motor 4 (top back right)
-            [-1,     -1,       0,      0,       0,      0,      0,         0], # motor 5 (bottom back right)
-            [ 0,      0,       1,     -1,      -1,      1,     -1,         1], # motor 6 (top front right)
-            [ 1,     -1,       0,      0,       0,      0,      0,         0]  # motor 7 (bottom front right)
+           #strafe for/bck   yaw     ptch#     up#    down#   Lroll#     Rroll#
+            [ 0,      0,       0,     -1,       1,      1,      1,        -1], # motor 0 (top front left)
+            [ 1,     -1,      -1,      0,       0,      0,      0,         0], # motor 1 (bottom front left)
+            [ 0,      0,       0,      1,      -1,      1,      1,        -1], # motor 2 (top back left)
+            [-1,     -1,      -1,      0,       0,      0,      0,         0], # motor 3 (bottom back left)
+            [ 0,      0,       0,      1,      -1,      1,     -1,         1], # motor 4 (top back right)
+            [-1,     -1,       1,      0,       0,      0,      0,         0], # motor 5 (bottom back right)
+            [ 0,      0,       0,     -1,       1,      1,     -1,         1], # motor 6 (top front right)
+            [ 1,     -1,       1,      0,       0,      0,      0,         0]  # motor 7 (bottom front right)
         ]
         self.input_list = [0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -52,13 +52,17 @@ class Can_Wrapper:
         if (value < 0):
             value = 255 - abs(value)
         return value
+        
     def move_forward(self, value):
-        self.input_list[1] = self.clamp(self.input_list[1] + value)
+        print(f"Move Forward {value}")
+        self.input_list[1] = self.clamp(self.input_list[1] + -value)
         
     def move_backward(self, value):
-        self.input_list[1] = self.clamp(self.input_list[1] + -value)
+        print("Move Backward")
+        self.input_list[1] = self.clamp(self.input_list[1] + value)
 
     def move_left(self, value):
+        
         self.input_list[0] = self.clamp(self.input_list[0] + value)
 
     def move_right(self, value):
@@ -71,21 +75,27 @@ class Can_Wrapper:
         self.input_list[5] = self.clamp(self.input_list[5] + value)
 
     def turn_up(self, value):
+        print("Turn Up")
         self.input_list[3] = self.clamp(self.input_list[3] + value)
 
     def turn_down(self, value):
-        self.input_list[4] = self.clamp(self.input_list[4] + -value)
+        print("Turn Down")
+        self.input_list[4] = self.clamp(self.input_list[4] + value)
 
     def turn_left(self, value):
+        print("Turn Left")
         self.input_list[2] = self.clamp(self.input_list[2] + value)
 
     def turn_right(self, value):
+        print("Turn Right")
         self.input_list[2] = self.clamp(self.input_list[2] + -value)
 
 
     def stop(self):
         self.input_list = [0,0,0,0,0,0,0,0]
 
+    def reset(self):
+        self.input_list = [0,0,0,0,0,0,0,0]
 
     def send_command(self):
         thrust_list = []
@@ -97,105 +107,124 @@ class Can_Wrapper:
         for motor_value_from_list in thrust_list:
             motor_value = self.twos_complement(int(self.clamp(motor_value_from_list)))
             command += '{:02X}'.format(motor_value) + " "
+        try:
+            message = can.Message(arbitration_id = 16, is_extended_id = False, data = bytearray.fromhex(command))
+            self.bus.send(message)
+            self.reset()
+        except:
+            print("NON-HEX", command)
 
-        message = can.Message(arbitration_id = 16, is_extended_id = False, data = bytearray.fromhex(command))
-        self.bus.send(message)
-        self.stop()
-
-
-if __name__ == "__main__":
+#main for testing
+def main():
     wrapper = Can_Wrapper()
-    #-----------------------------forward
-    wrapper.move_forward(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.move_backward(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------backward
-    wrapper.move_backward(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.stop()
-    wrapper.send_command()
-    time.sleep(1)
+    # #-----------------------------forward
+    # print("move Forward")
+    # wrapper.move_forward(.1)
+    # wrapper.send_command()
+    # time.sleep(10)
+    # #-----------------------------stop
+    # wrapper.move_backward(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------backward
+    # print("move backward")
+    # wrapper.move_backward(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.stop()
+    # wrapper.send_command()
+    # time.sleep(1)
 
 
 
-    #----------------------------left
-    wrapper.move_left(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.move_right(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------right
-    wrapper.move_right(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.stop()
-    wrapper.send_command()
-    time.sleep(1)
+    # #----------------------------left
+    # print("Move Left")
+    # wrapper.move_left(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.move_right(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------right
+    # print("move Right")
+    # wrapper.move_right(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.stop()
+    # wrapper.send_command()
+    # time.sleep(1)
 
 
-    #-----------------------------up
-    wrapper.move_up(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.move_down(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------down
-    wrapper.move_down(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.stop()
-    wrapper.send_command()
-    time.sleep(1)
+    # #-----------------------------up
+    # print("Move Up")
+    # wrapper.move_up(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.move_down(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------down
+    # print("Move Down")
+    # wrapper.move_down(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.stop()
+    # wrapper.send_command()
+    # time.sleep(1)
 
 
-    #-----------------------------pitch up
-    wrapper.turn_up(.1)
-    wrapper.send_command()
-    time.sleep(1)
+    # #-----------------------------pitch up
+    # print("Pitch Up")
+    # wrapper.turn_up(.1)
+    # wrapper.send_command()
+    # time.sleep(10)
 
-    #-----------------------------stop
-    wrapper.turn_down(.1)
-    wrapper.send_command()
-    time.sleep(1)
+    # #-----------------------------stop
+    # wrapper.turn_down(.1)
+    # wrapper.send_command()
+    # time.sleep(1)
 
-    #-----------------------------pitch down
-    wrapper.turn_down(.1)
-    wrapper.send_command()
-    time.sleep(1)
-    #-----------------------------stop
-    wrapper.stop()
-    wrapper.send_command()
-    time.sleep(1)
+    # #-----------------------------pitch down
+    # print("PiTcH DoWn")
+    # wrapper.turn_down(.1)
+    # wrapper.send_command()
+    # time.sleep(10)
+    # #-----------------------------stop
+    # wrapper.stop()
+    # wrapper.send_command()
+    # time.sleep(1)
 
     #-----------------------------turn left
+    print("TURN left")
     wrapper.turn_left(.1)
     wrapper.send_command()
-    time.sleep(1)
+    time.sleep(10)
     #-----------------------------stop
     wrapper.turn_right(.1)
     wrapper.send_command()
     time.sleep(1)
     #-----------------------------turn right
+    print("turn RIGHT")
     wrapper.turn_right(.1)
     wrapper.send_command()
-    time.sleep(1)
+    time.sleep(10)
     #-----------------------------stop
     wrapper.stop()
     wrapper.send_command()
     time.sleep(1)
 
     #-----------------------------stop
+    print("STo0o0o0o0o0P")
     wrapper.stop()
     wrapper.send_command()
+
+
+
+
+if __name__ == "__main__":
+    main()

7d137057fe2b014a39eb2e47ef0914f8a078f1b2
Samantha Georges <98787067+thesamgeorges@users.noreply.github.com>
2024-10-08 20:08:39 -0700

    swag motor_interface

--- MotorInterface.py
+++ MotorInterface.py
@@ -1,5 +1,6 @@
-from motors.MotorWrapper import Can_Wrapper
-from multiprocessing import Process, Value
+from motors.MotorWrapper            import Can_Wrapper
+from multiprocessing                import Process, Value
+from motors.ObjectTracking          import Object_Tracking
 import math
 import time
 import numpy as np
@@ -11,7 +12,6 @@ class MotorInterface:
     def __init__(self, shared_memory_object, x_hard_deadzone):
         self.shared_memory_object = shared_memory_object
         
-
         self.previous_x_yolo_offsets = []
 
         # self.max_iterations = 10000000
@@ -37,7 +37,7 @@ class MotorInterface:
 
         self.iterations = 0
         self.max_iterations = 100 + 200
-        #timeout / detection parameters------------------------------
+        #timeout / detection parameters-------------------------------------------------------
 
         self.iteration_since_last_detection = 0
         self.iterations_before_detection_timeout = 100
@@ -48,140 +48,9 @@ class MotorInterface:
         self.wait_length = 10
         self.current_wait = 0
 
-    def follow_buoy(self):   
-        print(self.shared_memory_object.color_offset[0].value )         
-        #NO OBJECT -------------------------------------------------
-        if self.shared_memory_object.color_offset[0].value == 0 and self.shared_memory_object.imu_orientation[0].value < .1:
-            self.iteration_since_last_detection += 1
-            return
-        #HARD DEADZONE----------------------------------------------
-        #turn right hard deadzone
-        #turn right if to the left of the hard deadzone
-        elif(self.shared_memory_object.color_offset[0].value < -self.x_hard_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed / 3))
-            self.iteration_since_last_detection = 0
-        #turn left hard deadzone
-        #turn left if to the right of the hard deadzone
-        elif (self.shared_memory_object.color_offset[0].value > self.x_hard_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed / 3))
-            self.iteration_since_last_detection = 0
-        #SOFT DEADZONE----------------------------------------------
-        #turn right soft deadzone
-        #turn right and move forward if to the left of the soft deadzone
-        elif (self.shared_memory_object.color_offset[0].value < -self.x_soft_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-        #turn left soft deadzone
-        #turn left and move forward if to the right of the soft deadzone
-        elif (self.shared_memory_object.color_offset[0].value > self.x_soft_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-        #CENTERED---------------------------------------------------
-        #move forward if inside soft deadzone    
-        else: 
-            #print("centered")
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-
-    def follow_gate(self):   
-        print("Offset x", self.shared_memory_object.gate_offset[0].value )         
-        #NO OBJECT -------------------------------------------------
-        if self.shared_memory_object.gate_offset[0].value == 0 and self.shared_memory_object.imu_orientation[0].value < .1:
-            self.iteration_since_last_detection += 1
-            return
-        #HARD DEADZONE----------------------------------------------
-        # #turn right hard deadzone
-        # #turn right if to the left of the hard deadzone
-        elif(self.shared_memory_object.gate_offset[0].value < -self.x_hard_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.iteration_since_last_detection = 0
-        # #turn left hard deadzone
-        # #turn left if to the right of the hard deadzone
-        elif (self.shared_memory_object.gate_offset[0].value > self.x_hard_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.iteration_since_last_detection = 0
-        #SOFT DEADZONE----------------------------------------------
-        #turn right soft deadzone
-        #turn right and move forward if to the left of the soft deadzone
-        elif (self.shared_memory_object.gate_offset[0].value < -self.x_soft_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-        #turn left soft deadzone
-        #turn left and move forward if to the right of the soft deadzone
-        elif (self.shared_memory_object.gate_offset[0].value > self.x_soft_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-        #CENTERED---------------------------------------------------
-        #move forward if inside soft deadzone    
-        else: 
-            #print("centered")
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-
-
-
-    def follow_yolo(self):            
-            #NO OBJECT -------------------------------------------------
-            if self.shared_memory_object.yolo_offset[0].value == 0.0 or self.shared_memory_object.distance_from_object.value > 10000:
-                self.iteration_since_last_detection += 1
-                return
-            #HARD DEADZONE----------------------------------------------
-            #turn right hard deadzone
-            #turn right if to the left of the hard deadzone
-            elif(self.shared_memory_object.yolo_offset[0].value < -self.x_hard_deadzone):
-                self.can.turn_right(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            #turn left hard deadzone
-            #turn left if to the right of the hard deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value > self.x_hard_deadzone):
-                self.can.turn_left(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            #SOFT DEADZONE----------------------------------------------
-            #turn right soft deadzone
-            #turn right and move forward if to the left of the soft deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value < -self.x_soft_deadzone):
-                self.can.turn_right(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-                self.can.move_forward(self.speed)
-            #turn left soft deadzone
-            #turn left and move forward if to the right of the soft deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value > self.x_soft_deadzone):
-                self.can.turn_left(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-                self.can.move_forward(self.speed)
-            #CENTERED---------------------------------------------------
-            #move forward if inside soft deadzone    
-            else: 
-                #print("centered")
-                self.can.move_forward(self.speed)
-            #STOP DEPTH-----------------------------------------------
-            self.iteration_since_last_detection = 0
-            #stop if depth is less than stop value
-            if (self.shared_memory_object.distance_from_object.value < self.distance_stop_value and self.shared_memory_object.distance_from_object.value != 0.0):
-                print("stop")
-                self.move_forward(1)
-                self.shared_memory_object.enable_color.value = True
-                self.shared_memory_object.enable_yolo.value = False
-
-
-    def move_down(self, down_time):
-        self.can.move_down(20)
-        self.can.send_command()
-        time.sleep(down_time)
-
-    def move_forward(self, forward_time):
-        self.can.move_forward(20)
-        self.can.move_down(.3)
-        self.can.send_command()
-        time.sleep(forward_time)
-
     def look_for_detection(self):
         if self.shared_memory_object.lin_ang_vel[0].value > self.y_turn_speed:
             return
-        
         if self.detection_thrust_count <= self.detection_thrust_length:
             self.can.turn_right(1)
             self.detection_thrust_count += 1
@@ -195,7 +64,6 @@ class MotorInterface:
             self.detection_thrust_count = 0
 
     def sit_at_depth(self):
-        # print(self.shared_memory_object.depth.value)
         if self.shared_memory_object.depth.value < self.min_depth:
             self.can.move_down(.5)
         elif self.shared_memory_object.depth.value > self.max_depth:
@@ -203,7 +71,6 @@ class MotorInterface:
         pass
 
     def face_direction(self, target):
-        #print(target - self.orientation_y.value)
         if self.shared_memory_object.imu_orientation[1].value < -.001:
             self.can.turn_left(min(abs(target - self.shared_memory_object.imu_orientation[1].value) * 3, 1))
             print("Correcting to the left")
@@ -212,7 +79,6 @@ class MotorInterface:
             print("Correcting to the right")  
 
     def correct_pitch(self):
-        # print(self.shared_memory_object.imu_orientation[0].value)
         if self.shared_memory_object.imu_orientation[0].value < -.001:
             self.can.turn_up(min(abs(self.shared_memory_object.imu_orientation[0].value) * 3, 1))
         elif self.shared_memory_object.imu_orientation[0].value > .001:
@@ -242,10 +108,10 @@ class MotorInterface:
         self.corrected_drift = True   
 
     def run_loop(self):
-
+        self.iterations = Object_Tracking.iterations()
+        self.iteration_since_last_detection = Object_Tracking.iteration_since_last_detection()
         while self.shared_memory_object.imu_lin_acc[0].value == 0.0: #wait for imu to start
             pass
-
         while self.iterations < 100 and self.shared_memory_object.running.value:
             start = time.time()
             self.sit_at_depth()
@@ -259,35 +125,30 @@ class MotorInterface:
 
         while self.shared_memory_object.running.value:   
             start = time.time()
-
             if (self.iteration_since_last_detection > self.iterations_before_detection_timeout) and self.enable_yolo:
                 self.look_for_detection()
             else:
                 self.current_wait = 0
                 self.detection_thrust_count = 0
-
             if self.shared_memory_object.gate_enable.value:
                 print("Offset: ", self.shared_memory_object.color_offset[0])
-                self.follow_gate()
+                self.Object_Tracking.follow_gate()
             if self.shared_memory_object.enable_yolo.value:
                 self.iterations += 1
                 self.look_for_detection()
-                self.follow_buoy()
+                self.Object_Tracking.follow_buoy()
                 
             self.sit_at_depth()
             print("Sitting at depth")
-
             if self.iterations == self.max_iterations and self.iterations - self.max_iterations < 50:
                 self.can.move_forward(3)
                 self.shared_memory_object.enable_color.value = False
                 self.shared_memory_object.color_offset[0].value = 0
-
                 pass
             if self.iterations == self.max_iterations:
                 self.can.stop()
                 self.shared_memory_object.enable_yolo.value = True
                 self.shared_memory_object.enable_color.value = False
-
             end = time.time()
             if (.05 - (end - start)) > 0:
                 time.sleep(.05 - (end - start))

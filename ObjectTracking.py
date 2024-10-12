6323805bc121a633d361fdbd5832dd19cbd6a259
joelofrese <joelofrese01@gmail.com>
2024-10-11 20:18:23 -0700

    Merged with tomato soup

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -1,113 +1,50 @@
-from motors.MotorWrapper            import Can_Wrapper
-from motors.MotorInterface          import MotorInterface
-from multiprocessing                import Process, Value
-import math
-import time
-import numpy as np
-
 class Object_Tracking:
 
-    def follow_object(self, color_offset, gate_offset, yolo_offset):
+    def __init__(self):
+        self.iterations = 0
+        self.iteration_since_last_detection = 0
+        #--------------------Deadzones--------------------------------
+        # Unit: number of pixels from the center of an image.
+        # When looking for the gate, if the center of the gate is outside of this value, turn towards gate only.
+        self.x_hard_deadzone = 400
+        # When looking for the gate, if the center of the gate is outside of this value,
+        # but inside the hard_deadzone value, turn towards gate and move forward.
+        # If inside of this value, move foward only.
+        self.x_soft_deadzone = 200
+
+    def follow_object(self, offset_to_follow, current_orientation, current_distance):
+
+        #-------------------------------------- NO OBJECT -------------------------------------------------
+        if offset_to_follow == 0 and current_orientation < .1 or current_distance > 1000:
+            self.iteration_since_last_detection += 1
+            return
         
-
-
-    def follow_buoy(self):   
-        print(self.shared_memory_object.color_offset[0].value )
-        #NO OBJECT
-        if self.shared_memory_object.color_offset[0].value == 0 and self.shared_memory_object.imu_orientation[0].value < .1:
-            self.iteration_since_last_detection += 1
-            return
-        #HARD DEADZONE
-        #turn right if to the left of the hard deadzone
-        elif(self.shared_memory_object.color_offset[0].value < -self.x_hard_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed / 3))
-            self.iteration_since_last_detection = 0
+        #------------------------------------- HARD DEADZONE ---------------------------------------------
+        # Turn right if to the left of the hard deadzone
+        elif(offset_to_follow < -self.x_hard_deadzone):
+            # Turn proportional to how far away the center of the buoy is from the center of the image.
+            self.can.turn_right(abs(offset_to_follow / self.normalizer_value * self.x_turn_speed))
         #turn left if to the right of the hard deadzone
-        elif (self.shared_memory_object.color_offset[0].value > self.x_hard_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed / 3))
-            self.iteration_since_last_detection = 0
-        #SOFT DEADZONE
-        #turn right and move forward if to the left of the soft deadzone
-        elif (self.shared_memory_object.color_offset[0].value < -self.x_soft_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed))
+        elif (offset_to_follow > self.x_hard_deadzone):
+            # Turn proportional to how far away the center of the buoy is from the center of the image.
+            self.can.turn_left(abs(offset_to_follow / self.normalizer_value * self.x_turn_speed))
+        
+        #--------------------------------- SOFT DEADZONE ----------------------------------------------
+        # Turn right and move forward if to the left of the soft deadzone
+        elif (offset_to_follow < -self.x_soft_deadzone):
+            # Turn proportional to how far away the center of the buoy is from the center of the image.
+            self.can.turn_right(abs(offset_to_follow / self.normalizer_value * self.x_turn_speed))
             self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-        #turn left and move forward if to the right of the soft deadzone
-        elif (self.shared_memory_object.color_offset[0].value > self.x_soft_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.color_offset[0].value / self.normalizer_value * self.x_turn_speed))
+        # Turn left and move forward if to the right of the soft deadzone
+        elif (offset_to_follow > self.x_soft_deadzone):
+            # Turn proportional to how far away the center of the buoy is from the center of the image.
+            self.can.turn_left(abs(offset_to_follow / self.normalizer_value * self.x_turn_speed))
             self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-        #CENTERED
-        #move forward if inside soft deadzone    
+
+        #------------------------------------ CENTERED ---------------------------------------------------
         else: 
             self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
+            if ( offset_to_follow == self.shared_memory_object.gate_offset[0]):
+                self.iterations += 1
 
-    def follow_gate(self):   
-        print("Offset x", self.shared_memory_object.gate_offset[0].value)
-        #NO OBJECT 
-        if self.shared_memory_object.gate_offset[0].value == 0 and self.shared_memory_object.imu_orientation[0].value < .1:
-            self.iteration_since_last_detection += 1
-            return
-        #HARD DEADZONE
-        #turn right if to the left of the hard deadzone
-        elif(self.shared_memory_object.gate_offset[0].value < -self.x_hard_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.iteration_since_last_detection = 0
-        #turn left if to the right of the hard deadzone
-        elif (self.shared_memory_object.gate_offset[0].value > self.x_hard_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.iteration_since_last_detection = 0
-        #SOFT DEADZONE
-        #turn right and move forward if to the left of the soft deadzone
-        elif (self.shared_memory_object.gate_offset[0].value < -self.x_soft_deadzone):
-            self.can.turn_right(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-        #turn left and move forward if to the right of the soft deadzone
-        elif (self.shared_memory_object.gate_offset[0].value > self.x_soft_deadzone):
-            self.can.turn_left(abs(self.shared_memory_object.gate_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-        #CENTERED
-        #move forward if inside soft deadzone    
-        else: 
-            self.can.move_forward(self.speed)
-            self.iteration_since_last_detection = 0
-            self.iterations += 1
-
-    def follow_yolo(self):            
-            #NO OBJECT ---
-            if self.shared_memory_object.yolo_offset[0].value == 0.0 or self.shared_memory_object.distance_from_object.value > 10000:
-                self.iteration_since_last_detection += 1
-                return
-            #HARD DEADZONE
-            #turn right if to the left of the hard deadzone
-            elif(self.shared_memory_object.yolo_offset[0].value < -self.x_hard_deadzone):
-                self.can.turn_right(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            #turn left if to the right of the hard deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value > self.x_hard_deadzone):
-                self.can.turn_left(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-            #SOFT DEADZONE
-            #turn right and move forward if to the left of the soft deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value < -self.x_soft_deadzone):
-                self.can.turn_right(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-                self.can.move_forward(self.speed)
-            #turn left and move forward if to the right of the soft deadzone
-            elif (self.shared_memory_object.yolo_offset[0].value > self.x_soft_deadzone):
-                self.can.turn_left(abs(self.shared_memory_object.yolo_offset[0].value / self.normalizer_value * self.x_turn_speed))
-                self.can.move_forward(self.speed)
-            #CENTERED
-            #move forward if inside soft deadzone    
-            else: 
-                self.can.move_forward(self.speed)
-            #STOP DEPTH
-            self.iteration_since_last_detection = 0
-            #stop if depth is less than stop value
-            if (self.shared_memory_object.distance_from_object.value < self.distance_stop_value and self.shared_memory_object.distance_from_object.value != 0.0):
-                print("stop")
-                self.move_forward(1)
-                self.shared_memory_object.enable_color.value = True
-                self.shared_memory_object.enable_yolo.value = False
+        self.iteration_since_last_detection = 0
